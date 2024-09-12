1f82daa72e7a681ea5e4ab8b0e8bea2b874085c7
alexander anciferov <sania.antsiferoff@yandex.ru>
2024-09-12 19:43:48 +0800

    update

--- config/settings.py
+++ config/settings.py
@@ -78,10 +78,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Project1',
+        'USER': 'postgres',
+        'PASSWORD': '123454321',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
+
+
 
 
 # Password validation
@@ -106,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
